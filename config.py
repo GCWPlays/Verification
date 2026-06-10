488fde83809b5d821d7aef10d8ba0ea2461d4337
GCWPlays <haidaralitab.2013@gmail.com>
2026-06-10 19:08:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN = ""
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+import os
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
